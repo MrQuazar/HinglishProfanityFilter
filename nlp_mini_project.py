68a6c000e053a1192bf5bbe33f87307c3fa120a6
Aartem Singh <70425120+MrQuazar@users.noreply.github.com>
2023-01-11 21:33:43 +0530

    added hindi profanity filter abilities

--- nlp_mini_project.py
+++ nlp_mini_project.py
@@ -1,4 +1,3 @@
-
 import nltk
 
 from nltk.tokenize import RegexpTokenizer
@@ -6,46 +5,47 @@ import re
 import pandas as pd
 import numpy as np
 
+
 class ProfHinglishFilter:
+    def __init__(self, gaali):
 
-  def __init__(self, gaali):
+        self.input_text = gaali
+        self.gaaliSent = gaali
+        # self.gaaliSent = re.sub(r'[^a-zA-Z0-9 ]',r'',self.input_text)
+        self.gaaliSent = self.gaaliSent.lower()
+        print(self.gaaliSent)
 
-    self.input_text = gaali
-    self.gaaliSent = re.sub(r'[^a-zA-Z0-9 ]',r'',self.input_text)
-    self.gaaliSent = self.gaaliSent.lower()
-    print(self.gaaliSent)
+    def filter_gaali(self):
+        # tokenizer = RegexpTokenizer(r'\w+')
 
-  def filter_gaali(self):
-    tokenizer = RegexpTokenizer(r'\w+')
+        # word_tokinize = tokenizer.tokenize(self.gaaliSent)
+        word_tokinize = self.gaaliSent.split()
+        print(word_tokinize)
 
-    word_tokinize = tokenizer.tokenize(self.gaaliSent)
+        data_set = pd.read_csv('profanity_filter.csv')
 
-    print(word_tokinize)
+        data_set.head()
 
-    data_set = pd.read_csv('profanity_filter.csv')
+        new = data_set.to_numpy()
+        new = new.flatten()
+        new
 
-    data_set.head()
+        output = []
 
-    new = data_set.to_numpy()
-    new = new.flatten()
-    new
+        for i in range(0, len(word_tokinize)):
+            found = True
+            for j in range(0, len(new)):
+                if word_tokinize[i] == new[j]:
+                    found = False
+                    temp = word_tokinize[i].replace(word_tokinize[i], "***")
+                    output.append(temp)
+                    break
+            if (found):
+                output.append(word_tokinize[i])
 
-    output = []
+        print(output)
 
-    for i in range(0, len(word_tokinize)):
-      found = True
-      for j in range(0, len(new)):
-        if word_tokinize[i]==new[j]:
-          found = False
-          temp = word_tokinize[i].replace(word_tokinize[i], "***")
-          output.append(temp)
-          break
-      if(found):
-          output.append(word_tokinize[i])
+        final_text = " ".join(output)
 
-    print(output)
-
-    final_text = " ".join(output)
-
-    print(final_text.title())
-    return [str(final_text.title()),str(self.gaaliSent)]
+        print(final_text.title())
+        return [str(final_text.title()), str(self.gaaliSent)]
